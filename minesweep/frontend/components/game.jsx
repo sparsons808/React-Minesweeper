8a629a4c2e2326f0aae95e63715d9de93d1d9cf5
Sydney <sydneyparsonsyb@gmail.com>
2021-10-18 11:11:11 -0400

    minesweeper

---
+++ minesweep/frontend/components/game.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+// import * as Minesweeper from '../../minesweeper';
+
+class Game extends React.Component {
+    constructor(){
+        super()
+    }
+
+    render() {
+        return (
+            <h3>React is working</h3>
+        )
+    }
+}
+
+export default Game;
